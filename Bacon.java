28a5457701844d52fd7370b6757b963693b00d1d
lamchiminhmark <34977592+lamchiminhmark@users.noreply.github.com>
2018-05-11 01:59:07 -0400

    Update Bacon.java

--- Bacon.java
+++ Bacon.java
@@ -16,123 +16,156 @@ import java.util.Set;
 import java.util.TreeMap;
 
 public class Bacon {
-    private static Map<String, List<String>> aTM = new HashMap<String, List<String>>(); // Map with key as String actors
-											// to value as List of
-    // movies
-    private static Map<String, List<String>> mTA = new HashMap<String, List<String>>(); // Map with key as String movie
-											// to value as List of
-    // actors
-    private static Set<String> visited = new HashSet<String>(); // Set of whether a string has been visited in a breadth
-								// first search
-    private static Map<String, String> predecessor = new HashMap<String, String>();
+	private static Map<String, List<String>> aTM = new HashMap<String, List<String>>(); // Map with key as String actors
+	// to value as List of
+	// movies
+	private static Map<String, List<String>> mTA = new HashMap<String, List<String>>(); // Map with key as String movie
+	// to value as List of
+	// actors
+	private static Map<String, String> predecessor = new HashMap<String, String>();
 
-    private static String center = "";
-    // @@@@ Made this queue static.
-    private static Queue<String> allEdgesTo = new LinkedList<String>();
+	private static String center = "";
 
-    public static void main(String[] args) {
-	// @@@@ Added generic type <String> to Lists.
-	Scanner s = null;
-	if (args[0].contains("http:")) {
-	    try {
-		s = new Scanner(new URL(args[0]).openStream());
-	    } catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace(); // change error message?
-	    }
-	} else {
-	    File file = new File(args[0]);
-	    try {
-		s = new Scanner(file);
-	    } catch (FileNotFoundException e) {
-		// TODO Auto-generated catch block
-		System.err.println(file + " is not found.");
-	    }
-	}
-	// This generates the aTM and mTA maps.
-	List<String> actors;
-	List<String> movies;
-	while (s.hasNextLine()) {
-	    String[] data = s.nextLine().split("\\|");
-	    String actor = data[0];
-	    // ???? Why do we need this?
-	    // if (!actor.contains("(")) {
-	    // actor = actor + "(I)";
-	    // }
-	    String movie = data[1];
-	    if (aTM.containsKey(actor)) {
-		List<String> currentList = aTM.get(actor);
-		currentList.add(movie);
-		aTM.put(actor, currentList);
-	    } else {
-		movies = new ArrayList<String>();
-		movies.add(movie);
-		aTM.put(actor, movies);
-	    }
-	    if (mTA.containsKey(movie)) {
-		List<String> currentList = mTA.get(movie);
-		currentList.add(actor);
-		mTA.put(movie, currentList);
-	    } else {
-		actors = new ArrayList<String>();
-		actors.add(actor);
-		mTA.put(movie, actors);
-	    }
-	}
+	public static void main(String[] args) {
+		// @@@@ Added generic type <String> to Lists.
+		Scanner s = null;
+		if (args[0].contains("http:")) {
+			try {
+				s = new Scanner(new URL(args[0]).openStream());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace(); // change error message?
+			}
+		} else {
+			File file = new File(args[0]);
+			try {
+				s = new Scanner(file);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				System.err.println(file + " is not found.");
+			}
+		}
+		// This generates the aTM and mTA maps.
+		List<String> actors;
+		List<String> movies;
+		while (s.hasNextLine()) {
+			String[] data = s.nextLine().split("\\|");
+			String actor = data[0];
+			// ???? Why do we need this?
+			// if (!actor.contains("(")) {
+			// actor = actor + "(I)";
+			// }
+			String movie = data[1];
+			if (aTM.containsKey(actor)) {
+				List<String> currentList = aTM.get(actor);
+				currentList.add(movie);
+				aTM.put(actor, currentList);
+			} else {
+				movies = new ArrayList<String>();
+				movies.add(movie);
+				aTM.put(actor, movies);
+			}
+			if (mTA.containsKey(movie)) {
+				List<String> currentList = mTA.get(movie);
+				currentList.add(actor);
+				mTA.put(movie, currentList);
+			} else {
+				actors = new ArrayList<String>();
+				actors.add(actor);
+				mTA.put(movie, actors);
+			}
+		}
 
-	// Determining center
-	if (args.length > 1) {
-	    for (int i = 1; i < args.length - 1; i++) {
-		center += args[i] + " ";
-	    }
-	    center += args[args.length - 1];
-	    if (!aTM.containsKey(center)) {
-		System.err.println(center + " is not in this data set.");
-		System.exit(1);
-	    }
-	} else if (!aTM.containsKey("Kevin Bacon (I)")) {
-	    center = aTM.keySet().toArray(new String[aTM.keySet().size()])[0];
-	} else {
-	    center = "Kevin Bacon (I)";
-	}
-	// Add the center to the first element of the queue in find.
-	allEdgesTo.add(center);
-	System.out.println("Center is: " + center);
-	// Additional commands prompts
-	help();
-	Scanner commands = new Scanner(System.in);
-	while (commands.hasNextLine()) {
-	    // Changed next() to nextLine().
-	    String input = commands.nextLine();
-	    if (input.equals("help")) {
+		// Determining center
+		if (args.length > 1) {
+			for (int i = 1; i < args.length - 1; i++) {
+				center += args[i] + " ";
+			}
+			center += args[args.length - 1];
+			if (!aTM.containsKey(center)) {
+				System.err.println(center + " is not in this data set.");
+				System.exit(1);
+			}
+		} else if (!aTM.containsKey("Kevin Bacon (I)")) {
+			center = aTM.keySet().toArray(new String[aTM.keySet().size()])[0];
+		} else {
+			center = "Kevin Bacon (I)";
+		}
+		// Add the center to the first element of the queue in find.
+		System.out.println("Center is: " + center);
+		findAllPredecessors();
+		// Additional commands prompts
 		help();
-	    } else if (input.contains("find")) {
-		String name = input.replaceFirst("find ", "");
-		find(name);
-		System.out.print("Enter a command: ");
-	    } else if (input.contains("recenter")) {
-		String name = input.replaceFirst("recenter ", "");
-		recenter(name);
-		System.out.print("Enter a command: ");
-	    } else if (input.equals("avgdist")) {
-		avgdist();
-		System.out.print("Enter a command: ");
-	    } else if (input.equals("circles")) {
-		circles();
-		System.out.print("Enter a command: ");
-	    } else if (input.contains("topcenter")) {
-		int n = Integer.parseInt(input.replaceFirst("topcenter ", ""));
-		topcenter(n);
-		System.out.print("Enter a command: ");
-	    } else if (input.equals("") || input.equals("quit")) {
-		System.out.println("Thank you for using our Kevin Bacon program!");
-		System.exit(0);
-
-	    } else {
-		System.out.print("Please reenter a command: ");
-	    }
+		Scanner commands = new Scanner(System.in);
+		while (commands.hasNextLine()) {
+			// Changed next() to nextLine().
+			String input = commands.nextLine();
+			if (input.equals("help")) {
+				help();
+			} else if (input.contains("find")) {
+				String name = input.replaceFirst("find ", "");
+				find(name);
+				System.out.print("Enter a command: ");
+			} else if (input.contains("recenter")) {
+				String name = input.replaceFirst("recenter ", "");
+				recenter(name);
+				System.out.print("Enter a command: ");
+			} else if (input.equals("avgdist")) {
+				avgdist();
+				System.out.print("Enter a command: ");
+			} else if (input.equals("circles")) {
+				circles();
+				System.out.print("Enter a command: ");
+			} else if (input.contains("topcenter")) {
+				int n = Integer.parseInt(input.replaceFirst("topcenter ", ""));
+				topcenter(n);
+				System.out.print("Enter a command: ");
+			} else if (input.equals("") || input.equals("quit")) {
+				System.out.println("Thank you for using our Kevin Bacon program!");
+				System.exit(0);
+			} else if (input.contains("topactors")) {
+				int n = Integer.parseInt(input.replaceFirst("topactors ", ""));
+				topActors(n);
+				System.out.print("Enter a command: ");
+			} else {
+				System.out.print("Please reenter a command: ");
+			}
+		}
 	}
-    }
+
+	private static void findAllPredecessors() {
+
+		/*
+		 * Finds all the nodes connected to the center and add entries to the hashmap
+		 * predecessor.
+		 */
+
+		Queue<String> allConnectedNodes = new LinkedList<String>();
+		Set<String> visited = new HashSet<String>();
+
+		allConnectedNodes.add(center);
+
+		while (!allConnectedNodes.isEmpty()) {
+			String currentNode = allConnectedNodes.remove();
+
+			// Mark the node visited.
+			List<String> currentEdgesTo;
+
+			// Get the appropriate list of vertices connected to target
+			if ((currentEdgesTo = aTM.get(currentNode)) == null)
+				currentEdgesTo = mTA.get(currentNode);
+
+			// For each unvisited connected vertex, update predecessor mark add to queue.
+			for (String vertex : currentEdgesTo) {
+				if (!visited.contains(vertex)) {
+					visited.add(vertex);
+					predecessor.put(vertex, currentNode);
+					allConnectedNodes.add(vertex);
+				}
+
+			}
+		}
+	}
 
     private static void topcenter(int n) {
 	// TODO Auto-generated method stub
@@ -249,124 +282,95 @@ public class Bacon {
     }
 
     private static void recenter(String name) {
-	center = name;
-	visited.clear();
-	predecessor.clear();
-	allEdgesTo.clear();
-	allEdgesTo.add(center);
-    }
-
-    private static int findBacon(String name) {
-
-	// If name is the center, return 0.
-	if (name.equals(center)) {
-	    return 0;
+		center = name;
+		predecessor.clear();
+		findAllPredecessors();
 	}
 
-	int bacon = -1;
-	String target = "";
+	private static int findBacon(String name) {
 
-	while (!target.equals(name) && !allEdgesTo.isEmpty()) {
-	    target = allEdgesTo.remove();
-
-	    // Mark the node visited.
-	    visited.add(target);
-	    List<String> currentEdgesTo;
-
-	    // Get the appropriate list of vertices connected to target
-	    if ((currentEdgesTo = aTM.get(target)) == null)
-		currentEdgesTo = mTA.get(target);
-
-	    // For each unvisited connected vertex, update predecessor mark add to queue.
-	    for (String vertex : currentEdgesTo) {
-		if (!visited.contains(vertex)) {
-		    predecessor.put(vertex, target);
-		    allEdgesTo.add(vertex);
+		// If name is the center, return 0.
+		if (name.equals(center)) {
+			return 0;
 		}
 
-	    }
-	}
-
-	if (predecessor.containsKey(name)) {
-	    String node = name;
-	    int count = 0;
-	    while (!node.equals(center)) {
-		node = predecessor.get(node);
-		count++;
-	    }
-	    // assign bacon to the count.
-	    bacon = count / 2;
-	}
-
-	// Returns the default -1 if connection not found.
-	return bacon;
-    }
-
-    private static int find(String name) {
-	// SUGGESTION: factor this check out for private method?
-	if (!aTM.containsKey(name)) {
-	    System.out.println(name + " is not in the database.");
-	    return -1;
-	}
-
-	// If name is the center, return 0.
-	if (name.equals(center)) {
-	    System.out.println(name + " (0)");
-	    return 0;
-	}
-
-	int bacon = -1;
-	String target = "";
-
-	while (!target.equals(name) && !allEdgesTo.isEmpty()) {
-	    target = allEdgesTo.remove();
-
-	    // Mark the node visited.
-	    visited.add(target);
-	    List<String> currentEdgesTo;
-
-	    // Get the appropriate list of vertices connected to target
-	    if ((currentEdgesTo = aTM.get(target)) == null)
-		currentEdgesTo = mTA.get(target);
-
-	    // For each unvisited connected vertex, update predecessor mark add to queue.
-	    for (String vertex : currentEdgesTo) {
-		if (!visited.contains(vertex)) {
-		    predecessor.put(vertex, target);
-		    allEdgesTo.add(vertex);
+		if (predecessor.containsKey(name)) {
+			String node = name;
+			int count = 0;
+			while (!node.equals(center)) {
+				node = predecessor.get(node);
+				count++;
+			}
+			// assign bacon to the count.
+			int bacon = count / 2;
+			return bacon;
 		}
 
-	    }
-	}
-	if (predecessor.containsKey(name)) {
-	    String node = name;
-	    int count = 0;
-	    while (!node.equals(center)) {
-		System.out.print(node + " -> ");
-		node = predecessor.get(node);
-		count++;
-	    }
-
-	    bacon = count / 2;
-	    System.out.println(node + " (" + bacon + ") ");
-	} else {
-	    System.out.println(name + " is unreachable.");
+		// Returns the default -1 if connection not found.
+		return -1;
 	}
 
-	// Returns the default -1 if connection not found.
-	return bacon;
-    }
+	private static void find(String name) {
+		// SUGGESTION: factor this check out for private method?
+		if (!aTM.containsKey(name)) {
+			System.out.println(name + " is not in the database.");
+			return;
+		}
 
-    private static void help() {
-	System.out.println("Available commands include: ");
-	System.out.println("   1. help");
-	System.out.println("   2. quit");
-	System.out.println("   3. find <name>");
-	System.out.println("   4. recenter <name>");
-	System.out.println("   5. avgdist");
-	System.out.println("   6. circles");
-	System.out.println("   7. topcenter <n>");
-	System.out.print("Enter a command: ");
+		int bacon = findBacon(name);
+		// If name is the center, return 0.
+		if (bacon < 0) {
+			System.out.println(name + " is unreachable.");
+		} else if (bacon == 0) {
+			System.out.println(name + " (0)");
+		} else {
+			String node = name;
+			while (!node.equals(center)) {
+				System.out.print(node + " -> ");
+				node = predecessor.get(node);
+			}
+			System.out.println(node + " (" + bacon + ") ");
+		}
+	}
 
-    }
+	private static void topActors(int n) {
+		class ReverseEntryComparator implements Comparator<Entry<String, Integer>> {
+
+			public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+				int val1 = o1.getValue();
+				int val2 = o2.getValue();
+				return val2 - val1;
+			}
+
+		}
+
+		Set<Entry<String, List<String>>> actors = aTM.entrySet();
+		PriorityQueue<Entry<String, Integer>> actorByMovie = new PriorityQueue<Entry<String, Integer>>(
+				new ReverseEntryComparator());
+
+		for (Entry<String, List<String>> actor : actors) {
+			String name = actor.getKey();
+			int moviesNo = actor.getValue().size();
+			actorByMovie.add(new AbstractMap.SimpleEntry<String, Integer>(name, moviesNo));
+		}
+
+		for (int i = 0; i < n; i++) {
+			Entry<String, Integer> top = actorByMovie.remove();
+			System.out.printf("%-10s %d\n", top.getKey(), top.getValue());
+		}
+	}
+
+	private static void help() {
+		System.out.println("Available commands include: ");
+		System.out.println("   1. help");
+		System.out.println("   2. quit");
+		System.out.println("   3. find <name>");
+		System.out.println("   4. recenter <name>");
+		System.out.println("   5. avgdist");
+		System.out.println("   6. circles");
+		System.out.println("   7. topcenter <n>");
+		System.out.println("   8. topactors <n>");
+		System.out.print("Enter a command: ");
+
+	}
 }
